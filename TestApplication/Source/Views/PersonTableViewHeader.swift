5992cdf866e2d19c0b7acbe3f9c3b84e7e819742
Ivanov Anton <evgenich95@gmail.com>
2016-06-07 22:27:02 +0700

    update PersonTableViewHeader.swift

--- TestApplication/Source/Views/PersonTableViewHeader.swift
+++ TestApplication/Source/Views/PersonTableViewHeader.swift
@@ -6,19 +6,43 @@
 //  Copyright © 2016 developer. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import SnapKit
 
 class PersonTableViewHeader: UITableViewHeaderFooterView {
 
-    @IBOutlet weak var sectionIcon: UIImageView!
-    
-    @IBOutlet weak var sectionName: UILabel!
+    var sectionIcon = UIImageView()
+    var sectionName = UILabel()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        createView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func createView() {
+        self.contentView.backgroundColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.3)
+        sectionName.textColor = UIColor.blueColor()
+
+        self.addSubview(sectionIcon)
+        self.addSubview(sectionName)
+
+        sectionIcon.snp_makeConstraints {(make) -> Void in
+            make.top.bottom.equalTo(self)
+            make.width.equalTo(sectionIcon.snp_height)
+            make.leading.equalTo(self).offset(8)
+        }
+
+        sectionName.snp_makeConstraints {(make) -> Void in
+            make.top.bottom.equalTo(self)
+            make.left.equalTo(sectionIcon.snp_right).offset(8)
+        }
+    }
 
     func updateUI(sectionName: String, sectionIconName: String) {
-        self.backgroundView = UIView(frame: self.bounds)
-        self.backgroundView?.backgroundColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.3)
-        
         sectionIcon.setImageWithoutCache(sectionIconName)
         self.sectionName.text = sectionName
     }
