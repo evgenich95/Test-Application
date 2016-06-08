911a3f92d1289a1c0540d695d4ab545257529100
Ivanov Anton <evgenich95@gmail.com>
2016-06-08 20:47:36 +0700

    create  enum PersonAttributeDescription

--- TestApplication/Helpers/Constants.swift
+++ TestApplication/Helpers/Constants.swift
@@ -16,6 +16,96 @@ struct UserInfoKeys {
     static let attributeOrderIndex = "orderIndex"
 }
 
+enum PersonAttributeDescription: CustomStringConvertible {
+    case FullName
+    case Salary
+    case EndVisitingHours
+    case StartVisitingHours
+    case EndMealTime
+    case StartMealTime
+    case WorkplaceNumber
+    case AccountantType
+
+    init?(attributeKey: String) {
+        switch attributeKey {
+        case "fullName":
+            self = .FullName
+        case "salary":
+            self = .Salary
+        case "endVisitingHours":
+            self = .EndVisitingHours
+        case "startVisitingHours":
+            self = .StartVisitingHours
+        case "endMealTime":
+            self = .EndMealTime
+        case "startMealTime":
+            self = .StartMealTime
+        case "workplaceNumber":
+            self = .WorkplaceNumber
+        case "accountantType":
+            self = .AccountantType
+        default:
+            return nil
+        }
+    }
+
+    var description: String {
+        switch self {
+        case FullName:
+            return "Full name:"
+        case Salary:
+            return "Salary:"
+        case EndVisitingHours:
+            return "EndVisitingHours"
+        case StartVisitingHours:
+            return "StartVisitingHours"
+        case EndMealTime:
+            return "End mealTime"
+        case StartMealTime:
+            return "Start meal time:"
+        case WorkplaceNumber:
+            return "Workplace number:"
+        case AccountantType:
+            return "Accountant type:"
+        }
+    }
+
+    var placeholder: String {
+        switch self {
+        case FullName:
+            return "Петров Сергей Иванович"
+        case Salary:
+            return "20000"
+        case AccountantType:
+            return "Material Accounting"
+        case WorkplaceNumber:
+            return "555"
+        case EndVisitingHours:
+            return "from 16:00 to 18:00"
+        case StartVisitingHours:
+            return "from 16:00 to 18:00"
+        case EndMealTime:
+            return "from 13:00 to 14:00"
+        case StartMealTime:
+            return "from 13:00 to 14:00"
+        }
+    }
+
+//    var type: NSAttributeType {
+//        switch self {
+//        case FullName:
+//            return NSAttributeType.StringAttributeType
+//        case Salary:
+//            return NSAttributeType.DoubleAttributeType
+//        case AccountantType,WorkplaceNumber:
+//            return "Material Accounting"
+//        case WorkplaceNumber:
+//            return "555"
+//        case StartMealTime,EndMealTime,StartVisitingHours,EndVisitingHours:
+//            return NSAttributeType.DateAttributeType
+//        }
+    }
+
 enum AccountantType: CustomStringConvertible {
     case SalaryAccounting
     case MaterialAccounting
